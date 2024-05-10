1935d41b4422ca602f0d3d37e60b96aca3b70ca2
Sergeygvl <sergeyg.vl@mail.ru>
2024-05-10 13:45:33 +0300

    Код изменён в соответствии с правками

--- Dihot.py
+++ Dihot.py
@@ -6,31 +6,35 @@ import matplotlib as mpl
 def function(x):
     return x**2 - 2*x*(4/9) + 16/81
 
-def Swann_algorythm(x0,t):
+def Swann_algorythm(x0, t, ismax):
     delta = 0
     power = 1
-    if (function(x0 - t) >= function(x0)) and (function(x0) <= function(x0 + t)):
+    if ismax == 0:
+        coeff = 1
+    else:
+        coeff = -1
+    if (function(x0 - t)*coeff >= function(x0)*coeff) and (function(x0)*coeff <= function(x0 + t)*coeff):
         return x0 - t, x0 + t
     
-    if (function(x0 - t) <= function(x0)) and (function(x0) >= function(x0 + t)):
+    if (function(x0 - t)*coeff <= function(x0)*coeff) and (function(x0)*coeff >= function(x0 + t)*coeff):
         raise ValueError("Функция не унимодальная, попробуйте выбрать другую начальную точку")
     
-    if (function(x0 - t) >= function(x0)) and (function(x0) >= function(x0 + t)):
+    if (function(x0 - t)*coeff >= function(x0)*coeff) and (function(x0)*coeff >= function(x0 + t)*coeff):
         delta = t
         a = x0
         x = x0 + t
     
-    if (function(x0 - t) <= function(x0)) and (function(x0) <= function(x0 + t)):
+    if (function(x0 - t)*coeff <= function(x0)*coeff) and (function(x0)*coeff <= function(x0 + t)*coeff):
         delta = -t
         b = x0
         x = x0 - t
 
-    while (function(x) < function(x0)):
+    while (function(x)*coeff < function(x0)*coeff):
         power *= 2 
         x0 = x
         x = power*delta
 
-        if (function(x) < function(x0)):
+        if (function(x)*coeff < function(x0)*coeff):
             if delta == t:
                 a = x0
             else:
@@ -42,16 +46,19 @@ def Swann_algorythm(x0,t):
                 a = x
     return a, b
 
-def dichotomy_method(f, a, b, epsilon, l):
+def dichotomy_method(f, a, b, epsilon, l, ismax):
     length = abs(b-a)
     k = 0
     points = [[a,b]]
-
+    if ismax == 0:
+        coeff = 1
+    else:
+        coeff = -1
     while length > l:
         y = (a + b - epsilon) / 2
         z = (a + b + epsilon) / 2
 
-        if f(y) <= f(z):
+        if f(y)*coeff <= f(z)*coeff:
             b = z
         else:
             a = y
@@ -61,8 +68,11 @@ def dichotomy_method(f, a, b, epsilon, l):
         points.append([a,b])
     return (a + b) / 2, k, points, a, b
 
-def golden_ratio_method(f, a, b, l):
-    
+def golden_ratio_method(f, a, b, l, ismax):
+    if ismax == 0:
+        coeff = 1
+    else:
+        coeff = -1
     delta = abs(b-a)
     k = 0
     golden_ratio = (3 - math.sqrt(5)) / 2
@@ -72,7 +82,7 @@ def golden_ratio_method(f, a, b, l):
         y = a + golden_ratio * (b - a)
         z = a + b - y
 
-        if f(y) <= f(z):
+        if f(y)*coeff <= f(z)*coeff:
             b = z
             y_new = a + b - y
             y = y_new
@@ -97,13 +107,29 @@ def drawplot(result, k, points):
         plt.plot([points[i][0],points[i][0]],[function(result) - 2 + i/k,function(points[i][0])], color = colors[i % n], linestyle='dashed', marker='o')
         plt.plot([points[i][1],points[i][1]],[function(result) - 2 + i/k,function(points[i][1])], color = colors[i % n], linestyle='dashed', marker='o')
     plt.show()
-
 x0 = 3
 t = 1
-a, b = Swann_algorythm(x0,t)
-result_dih, k, points, an, bn = dichotomy_method(function, a, b, 1e-3, 0.1)
+ismax = int(input("Введите 1 если ищется максимум функции и 0 для поиска минимума: "))
+if ismax != 0 and ismax != 1:
+    raise ValueError("Введённое значение может быть только 0 или 1")
+a, b = Swann_algorythm(x0, t, ismax)
+print(a,b)
+result_dih, k, points, an, bn = dichotomy_method(function, a, b, 1e-3, 0.1, ismax)
 drawplot(result_dih, k, points)
-print(f"Решение уравнения методом дихотомии: x = {result_dih}, a = {an}, b = {bn}, f(x) = {function(result_dih)}, k = {k-1}")
-result_gold, k, points, an, bn = golden_ratio_method(function, a, b, 0.1)
+print("Отрезки для метода дихотомии:")
+for i in range(k):
+    print("a{0} = {1}".format(i, points[i][0]))
+    print("b{0} = {1}".format(i, points[i][1]))
+    print("l{0} = {1}".format(i, points[i][1] - points[i][0]))
+    print('\n')
+print(f"Решение уравнения методом дихотомии: x = {result_dih}, f(x) = {function(result_dih)}, k = {k-1}")
+
+result_gold, k, points, an, bn = golden_ratio_method(function, a, b, 0.1, ismax)
 drawplot(result_gold, k, points)
-print(f"Решение уравнения методом золотого сечения: x = {result_gold}, a = {an}, b = {bn}, f(x) = {function(result_gold)}, k = {k-1}")
+print("Отрезки для метода золотого сечения:")
+for i in range(k):
+    print("a{0} = {1}".format(i, points[i][0]))
+    print("b{0} = {1}".format(i, points[i][1]))
+    print("l{0} = {1}".format(i, points[i][1] - points[i][0]))
+    print('\n')
+print(f"Решение уравнения методом золотого сечения: x = {result_gold}, f(x) = {function(result_gold)}, k = {k-1}")
